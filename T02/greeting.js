7c769f2649e005ff14a06589071686d9d0d474c4
JacksonGould <63510075+JacksonGould@users.noreply.github.com>
2022-11-17 17:47:57 +0000

    Add files via upload

---
+++ T02/greeting.js
@@ -0,0 +1,6 @@
+let firstName = prompt("Please enter your name: ");
+let age = prompt("Please enter your age: ");
+
+console.log("Hi " + firstName);
+console.log("You are " + age + " years old");
+console.log("It's nice to meet you!");
---
+++ T02/shopping.js
@@ -0,0 +1,8 @@
+let item1 = prompt ("Add an item to your shopping list: ");
+let item2 = prompt ("Please add another: ");
+let item3 = prompt ("and another: ");
+
+console.log ("Shopping List: ");
+console.log (item1);
+console.log (item2);
+console.log (item3);
