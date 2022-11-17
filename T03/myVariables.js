7ca59f4cec3ec3b8dbc50a317327e989f2a99696
JacksonGould <63510075+JacksonGould@users.noreply.github.com>
2022-11-17 17:52:42 +0000

    Add files via upload

---
+++ T03/myVariables.js
@@ -0,0 +1,26 @@
+//List of Variables, using let or const where appropriate:
+const myFirstNumber = 1;
+const mySecondNumber = 2;
+const myFirstString = "Hello";
+const mySecondString = "World!";
+let myBoolean = true;
+
+
+//Output of integers multiplied together
+console.log(myFirstNumber * mySecondNumber);
+
+//Output of strings joined together
+console.log(myFirstString + " " + mySecondString);
+
+
+//Assign Template Literal to string variable
+output = `\nThe boolean is: true
+The first number is: 1
+The second number is: 2
+1 x 2 = 2
+The first string is: "Hello"
+The second string is: "World!"
+These two together make: "Hello World!"`;
+
+//Output template literal
+console.log(output);
